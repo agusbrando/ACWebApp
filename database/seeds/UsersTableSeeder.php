ad0bd326e4cd6750fe3f1c5355196d90caf40d49
Ggarrido <guigarp1@gmail.com>
2020-03-26 19:57:20 +0100

    feat:Instalation and Configuration Laravel 7 MySQL Structure Header, welcome, base and aside

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@campusaula.com',
+            'password' => bcrypt('adminPass'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'User',
+            'email' => 'user@campusaula.com',
+            'password' => bcrypt('userPass'),
+        ]);
+    }
+}
---
+++ resources/views/base.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>AulaCampus Site</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/base.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/login.css') }}" rel="stylesheet" type="text/css" />
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">   
+        
+     </head>
+    <body>        
+        @include('header')
+        <div class="container-fluid">
+            <div class="row">
+                @include('aside')
+                @yield('main')
+            </div>
+        </div>        
+        <!-- JavaScripts -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+        <script src="{{ resource_path('js/dashboard.js') }}" type="text/js"></script>
+    </body>
+</html>
