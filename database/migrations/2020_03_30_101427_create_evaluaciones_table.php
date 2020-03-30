844e5f4aa8c5759d3076ec7d56f7becf324b509e
Javier Andres <javiamdazo@gmail.com>
2020-03-30 13:26:57 +0200

    Creacion de migraciones

---
+++ database/migrations/2020_03_30_101427_create_evaluaciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvaluacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluaciones', function (Blueprint $table) {
+            $table->id('id_evaluaciones');
+            $table->string('nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluaciones');
+    }
+}
---
+++ database/migrations/2020_03_30_101443_create_tareas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id('id_tareas');
+            $table->string('idAsignaturas');
+            $table->date('fecha_entrega');
+            $table->string('nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+}
