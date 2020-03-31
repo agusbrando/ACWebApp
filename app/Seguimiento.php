8905419548ef6bff6837c259c753b701ff3032e7
c4rloscu <carloscu1234321@gmail.com>
2020-03-31 12:13:53 +0200

    modelo

--- app/Seguimiento.php
+++ app/Seguimiento.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seguimiento extends Model
 {
-    //
+   protected $table ='seguimientos';
+
+   protected $primaryKey ='id';
+
+   protected $fillable = [
+    'id_usuario',
+    'hora_entrada',
+    'hora_salida',
+    'horas',
+    'fecha'
+
+   ]
+       
+   
 }
