85ce899d9394b64d5600f963268ade5d0eca097e
sefape <sefape@campusaula.com>
2020-04-01 13:43:22 +0200

    migracion de sends

---
+++ database/migrations/2020_04_01_6_sends.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Sends extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sends', function (Blueprint $table) {
+            $table->integer('message_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('read')->unsigned();
+            $table->timestamps();
+
+            $table->primary(['message_id','user_id']);
+            $table->foreign('message_id')->references('id')->on('messages');
+            $table->foreign('user_id')->references('id')->on('users');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sends');
+    }
+}
