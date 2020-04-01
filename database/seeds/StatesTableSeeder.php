f8fee884d8104928759871d7e6e3814b1442ae37
Sergio <serlomar@campusaula.com>
2020-04-01 18:19:38 +0200

    ref-Seed estados migrada a states

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -11,6 +11,35 @@ class StatesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('estados')->insert([
+            'name' => 'Averiado',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('estados')->insert([
+            'name' => 'Mantenimiento',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('estados')->insert([
+            'name' => 'Reparado',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('estados')->insert([
+            'name' => 'Sin Averias',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('estados')->insert([
+            'name' => 'Obsoleto',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('estados')->insert([
+            'name' => 'Pendiente de compra',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
