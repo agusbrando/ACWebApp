f9f1e12b1ff535af390ab8bc1e17fce8373cc649
kehocu <kehocu@campusaula.com>
2020-04-01 16:41:38 +0200

    Seed de users

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+        DB::table('users')->insert([
+            'first_name' => 'default',
+            'last_name' => 'default',
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Str::random(10),
+            'rol_id' => 1,
+        ]);
+        
+        DB::table('users')->insert([
+            'first_name' => 'default2',
+            'last_name' => 'default2',
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Str::random(10),
+            'rol_id' => 2,
+        ]);
+    }
+}
