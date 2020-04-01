3497537f946ec4707e2c592aa1cf033f06988abb
kehocu <kehocu@campusaula.com>
2020-04-01 16:41:10 +0200

    Seed de events

---
+++ database/seeds/EventsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EventsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('events')->insert([
+            'types_id' => 1,
+            'sessions_id' => 1,
+            'users_id' =>1,
+            'description' => 'default',
+            'date' => date("Y-m-d")
+        ]);
+
+        DB::table('events')->insert([
+            'types_id' => 2,
+            'sessions_id' => 2,
+            'users_id' =>2,
+            'description' => 'default',
+            'date' => date("Y-m-d")
+        ]);
+    }
+}
