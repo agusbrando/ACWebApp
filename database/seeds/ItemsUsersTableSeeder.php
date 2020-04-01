adfff65184017b1cf8e9255251a96f235455fb23
Sergio <serlomar@campusaula.com>
2020-04-01 18:27:20 +0200

    ref- Seed ItemsUsers traducida

--- database/seeds/ItemsUsersTableSeeder.php
+++ database/seeds/ItemsUsersTableSeeder.php
@@ -12,22 +12,20 @@ class ItemsUsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
-        //ERROR: NO CARGA LOS DATOS EN LA BD
-        //
+        
         DB::table('items-users')->insert([
-            'id_item' => 2,
-            'id_user' => 1,
-            'fecha_inicio' => Carbon::create('2019','09','16'),
-            'fecha_fin' => Carbon::create('2020','06','12'),
+            'type_id' => 2,
+            'user_id' => 1,
+            'date_inicio' => Carbon::create('2019','09','16'),
+            'date_fin' => Carbon::create('2020','06','12'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
         DB::table('items-users')->insert([
-            'id_item' => 1,
-            'id_user' => 2,
-            'fecha_inicio' => Carbon::create('2019','09','16'),
-            'fecha_fin' => Carbon::create('2020','06','12'),
+            'type_id' => 1,
+            'user_id' => 2,
+            'date_inicio' => Carbon::create('2019','09','16'),
+            'date_fin' => Carbon::create('2020','06','12'),
             'created_at' => now(),
             'updated_at' => now()
         ]);
