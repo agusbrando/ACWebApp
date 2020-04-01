faee8de674a5f9fdf04fcaba290abe794ccef778
Sergio <serlomar@campusaula.com>
2020-04-01 18:22:15 +0200

    ref- Seed TiposItem migrada a Types

--- database/seeds/TypesTableSeeder.php
+++ database/seeds/TypesTableSeeder.php
@@ -11,6 +11,35 @@ class TypesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('types')->insert([
+            'model' => 'Item',
+            'nombre' => 'Teclado',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('types')->insert([
+            'model' => 'Item',
+            'nombre' => 'All in One',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('types')->insert([
+            'model' => 'Item',
+            'nombre' => 'Raton',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('types')->insert([
+            'model' => 'Item',
+            'nombre' => 'Aire Acondicionado',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('types')->insert([
+            'model' => 'Item',
+            'nombre' => 'Portatil',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
