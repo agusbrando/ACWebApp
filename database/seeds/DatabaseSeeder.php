ab7ff5f76e70da4a7668d326abb6e61f5c45dfde
kehocu <kehocu@campusaula.com>
2020-04-01 16:36:47 +0200

    Eliminacion de Database antigua

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class
+            TypesSeeder::class,
+            ClassesSeeder::class,
+            SessionsSeeder::class,
+            UsersSeeder::class,
+            EventsSeeder::class
         ]);
     }
 }
