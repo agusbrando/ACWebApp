266e383a3b97b1f374dee0e9297396ad0c80db33
sefape <sefape@campusaula.com>
2020-04-01 13:43:14 +0200

    migracion de attachments

---
+++ database/migrations/2020_04_01_5_attachments.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Attachments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('attachable_id')->unsigned();
+            $table->string('attachable_type');
+            $table->integer('messages_id')->unsigned();
+            $table->integer('post_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('messages_id')->references('id')->on('messages');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attachments');
+    }
+}
