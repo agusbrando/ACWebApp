194b56e2a3e5b1befd9ca71588f08d96008c0042
kehocu <kehocu@campusaula.com>
2020-04-02 11:57:58 +0200

    Relacion Sessions Model

--- app/Models/SessionsModel.php
+++ app/Models/SessionsModel.php
@@ -13,9 +13,19 @@ class SessionsModel extends Model
     protected $fillable = [
         'id',
         'classes_id',
+        'model',
         'time_start',
-        'time_end',
-        'model'
+        'time_end'
     ];
+
+    public function sessions()
+    {
+        return $this->hasOne('App\Models\SessionsModel', 'sessions_id');
+    }
+
+    public function classes()
+    {
+        return $this->belongsTo('App\Models\ClassesModel', 'classes_id');
+    }
 
 }
