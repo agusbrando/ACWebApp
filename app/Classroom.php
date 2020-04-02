9d8f97a2bdd5919c7786dc1965117c02b759138f
Sergio <serlomar@campusaula.com>
2020-04-02 16:36:36 +0200

    feat-Modelo Classroom creado

---
+++ app/Classroom.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom extends Model
+{
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'aulas';
+    // protected $table = 'classrooms';
+
+
+    /**
+    * The primary key associated with the table.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+    // protected $primaryKey = 'classroom_id';
+
+
+    /**
+    * Indicates if the IDs are auto-incrementing.
+    * Por defecto Eloquent asume que la clave primaria 
+    * es un valor incremental y de tipo int, 
+    * Si fuera de tipo string o no fuera auto incremental pondríamos false
+    * @var bool
+    */
+    public $incrementing = true;
+
+
+    /**
+    * The "type" of the auto-incrementing ID.
+    * Si tu clave primaria no es un entero, debes establecer 
+    * la propiedad protegida $keyType de tu modelo a string,
+    * como en este caso
+    * @var string
+    */
+    protected $keyType = 'string';
+
+
+    /**
+    * Indicates if the model should be timestamped.
+    *
+    * @var bool
+    */
+    public $timestamps = true;
+
+    // Asignación masiva
+
+    // Puedes usar el método create para guardar un nuevo modelo en una sola línea. 
+    // La instancia de modelo insertada te será devuelta por el método. Sin embargo, 
+    // antes de hacer eso, necesitarás especificar o un atributo fillable o guarded del modelo, 
+    // de modo que todos los modelos de Eloquent se protejan contra la asignación masiva de forma predeterminada.
+
+    protected $fillable =[
+        'id',
+        'nombre',
+        'num_aula',
+        'num_alumnos',
+
+    ];
+}
