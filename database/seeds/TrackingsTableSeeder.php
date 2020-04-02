68837aa502fa2a4a42be0c09ed2af378accec5a9
c4rloscu <carloscu1234321@gmail.com>
2020-04-02 11:49:12 +0200

    seeds añadidas

---
+++ database/seeds/TrackingsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TrackingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $trackings = [
+            [
+                'signature'=>'',
+                'user_id' => '3',
+                'datetime_start' =>  now(),
+                'datetime_end' => now(),
+                'num_hours'=>'4',
+                
+                
+            ],
+            [
+                'signature'=>'',
+                'user_id' => '4',
+                'datetime_start' =>  now(),
+                'datetime_end' =>  now(),
+                'num_hours'=>'3',
+                
+                
+            ],
+        ];
+        DB::table('trackings')->insert($trackings);
+    }
+}
