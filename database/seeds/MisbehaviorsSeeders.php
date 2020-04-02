af2937d9e47a31b7c1249fcdf7628238a1916b43
aferrerc <aferrerc24@gmail.com>
2020-04-02 12:35:37 +0200

    ref-Add seeds/MisbehaviorsSeeders

---
+++ database/seeds/MisbehaviorsSeeders.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MisbehaviorsSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('misbehaviors')->insert([
+            'id' => 1,
+            'description' => 'Retraso',
+            'type_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
