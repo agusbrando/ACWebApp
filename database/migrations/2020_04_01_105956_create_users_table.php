25e5fe21a064e0ab04d31ea80bd665a7b17399f4
c4rloscu <carloscu1234321@gmail.com>
2020-04-02 11:05:41 +0200

    migrations creadas

---
+++ database/migrations/2020_04_01_105956_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('password');
+            $table->integer('rol_id')->unsigned();
+            $table->timestamps();
+            
+            $table->foreign('rol_id')->references('id')->on('roles');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('users');
+    }
+}
