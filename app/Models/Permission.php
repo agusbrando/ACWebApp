02262b47cc058f99adcfba0ddd89a03eb2d4190f
roby_baltaretu <roberto.baltaretu@gmail.com>
2020-04-03 09:17:13 +0200

    Subo modelos

---
+++ app/Models/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    protected $table = 'permissions';
+    protected $pimaryKey = 'id_permission';
+    const CREATED_AT = 'Creacion';
+    const UPDATED_AT = 'Actualizacion';
+}
