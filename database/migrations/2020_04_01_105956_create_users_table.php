bd8aaaeeb08afa6f3b1da2c7e2df291e1f841daa
c4rloscu <carloscu1234321@gmail.com>
2020-04-03 16:45:20 +0200

    tablas de agustin fernandez añadidas

--- database/migrations/2020_04_01_105956_create_users_table.php
+++ database/migrations/2020_04_01_105956_create_users_table.php
@@ -18,12 +18,14 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password');
             $table->integer('rol_id')->unsigned();
+            $table->integer('timetable_id')->unsigned();
             $table->timestamps();
             
             $table->foreign('rol_id')->references('id')->on('roles');
+            $table->foreign('timetable_id')->references('id')->on('timetables');
             
         });
     }
---
+++ database/migrations/32020_03_30_101801_create_sessions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('classroom_id')->unsigned();
+            $table->dateTime('time_start', 0);
+            $table->dateTime('time_end', 0);
+            $table->string('model');
+            $table->timestamps();
+
+            $table->foreign('classroom_id')->references('id')->on('classrooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sessions');
+    }
+}
