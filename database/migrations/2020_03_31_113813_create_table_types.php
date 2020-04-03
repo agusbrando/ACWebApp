3c038e09f8ea6a373060a10404c0bb4c050aa83a
Sergio <serlomar@campusaula.com>
2020-04-03 11:14:59 +0200

    ref-Valor unico name asignado

--- database/migrations/2020_03_31_113813_create_table_types.php
+++ database/migrations/2020_03_31_113813_create_table_types.php
@@ -16,7 +16,7 @@ class CreateTableTypes extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('model');
-            $table->string('name');            
+            $table->string('name')->unique();            
             $table->timestamps();
         });
     }
