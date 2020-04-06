ea5777c1cc7d95cf7f94ddeb4e19eeb974771eed
jegagi <jegagi32@gmail.com>
2020-04-06 13:14:16 +0200

    fix: arreglo errores

--- tests/Feature/App/Models/UserTest.php
+++ tests/Feature/App/Models/UserTest.php
@@ -160,60 +160,10 @@ class UserTest extends TestCase
 
         $this->assertEquals($programs_ids,$expected_programs_ids);
 
-
-    }
-    public function creoDatos(){
-        $role = Role::create([
-            'name' => 'Profesor',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        $user_one_profesor = User::create([
-            'first_name' => 'Profesor',
-            'last_name' => 'Apellido Apellido',
-            'email' => 'profesor.apellido1@campusaula.com',
-            'password' => bcrypt('password'),
-            'created_at' => now(),
-            'updated_at' => now(),
-            'role_id' => $role->id
-        ]);
-        $user_two_responsable = User::create([
-            'first_name' => 'Profesor',
-            'last_name' => 'Apellido Apellido',
-            'email' => 'profesor.apellido2@campusaula.com',
-            'password' => bcrypt('password'),
-            'created_at' => now(),
-            'updated_at' => now(),
-            'role_id' => 3
-        ]);
-        $program_one = Program::create([
-            'date_check' => '2019-09-22',	
-            'notes' => 'Muy bien estructurado',
-            'subject_id' => 3,
-            'professor_id' => $user_one_profesor->id,
-            'user_id' => $user_two_responsable->id,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        $program_two = Program::create([
-            'date_check' => '2019-09-22',	
-            'notes' => 'Muy bien estructurado',
-            'subject_id' => 3,
-            'professor_id' => $user_one_profesor->id,
-            'user_id' => $user_two_responsable->id,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
         $program_two->delete();
         $program_one->delete();
         $user_two_responsable->delete();
         $user_one_profesor->delete();
     }
-    public function borroDatos(){
-        $program_two->delete();
-        $program_one->delete();
-        $user_two_responsable->delete();
-        $user_one_profesor->delete();
-        $role->delete();
-    }
+    
 }
