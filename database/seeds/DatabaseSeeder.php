a97be3d73293222b288e1a49873fc6832a04d431
Javier Andres <javiamdazo@gmail.com>
2020-04-06 12:29:53 +0200

    Modifico seeder users, añado seeder de timetable

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            TimetableTableSeeder::class,
             RolesTableSeeder::class,
             TypesTableSeeder::class,
             UsersTableSeeder::class,
