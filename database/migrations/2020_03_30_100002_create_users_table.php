14005611ebee16cc15d7bb60dd3e194196286a21
jegagi <jegagi32@gmail.com>
2020-04-06 10:04:37 +0200

    cambio en la tabla users rol_id por role_id

--- database/migrations/2020_03_30_100002_create_users_table.php
+++ database/migrations/2020_03_30_100002_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->integer('rol_id')->unsigned();
-            $table->foreign('rol_id')->references('id')->on('roles');
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('password');
             $table->string('email')->unique();
             $table->timestamps();
