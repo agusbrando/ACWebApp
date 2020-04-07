38cf51ae44416a63e5ac812670789fdf8183134b
Javier Andres <javiamdazo@gmail.com>
2020-04-07 12:02:57 +0200

    Test Evaluation con tasks

--- tests/Feature/EvaluationTest.php
+++ tests/Feature/EvaluationTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Task;
+use App\Models\Evaluation;
 
 class EvaluationTest extends TestCase
 {
@@ -20,7 +22,16 @@ class EvaluationTest extends TestCase
 
     public function testTasks()
     {
+        $evaluation = Evaluation::find(1);
+
+        $tasks = $evaluation->tasks->pluck('id');
         
+        $expected_tasks_ids = collect([
+            ['id' => 1],
+            ['id' => 2]
+        ])->pluck('id');
+
+        $this->assertEquals($tasks, $expected_tasks_ids);
     }
 
     public function testPercentages()
