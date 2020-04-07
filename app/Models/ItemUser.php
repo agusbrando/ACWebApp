15bc4ce1846b9eeecc122f757864f807e5d11f38
Sergio <serlomar@campusaula.com>
2020-04-07 13:57:04 +0200

    ref-Modelo renombrado

--- app/Models/ItemUser.php
+++ app/Models/ItemUser.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class item_user extends Pivot  //Al ser la tabla intermedia extiende de Pivot
+class ItemUser extends Pivot  //Al ser la tabla intermedia extiende de Pivot
 {
     protected $table = 'items-users';
     protected $guarded =[]; //En vez de poner fillable y todos los atributos,
