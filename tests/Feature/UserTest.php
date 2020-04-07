2c2f205d49a51afa7a400f050c5fd90a44e6c987
Agustín Brando Alonso <agubral@campusaula.com>
2020-04-07 10:53:21 +0200

    Remove user test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -16,41 +16,7 @@ class UserTest extends TestCase
     public function testExample()
     {
 
-        $type = Types::create([
-            'name' => 'default',
-            'model' => 'defaultModel'
-        ]);
 
-        $session = Sessions::create([
-            'classroom_id'=>$classroom->id,
-            'time_start' => date('Y-m-d H:i:s'),
-            'time_end' => date('Y-m-d H:i:s'),
-            'model' => 'defaultModel'
-        ]);
-
-        $user = Users::create([
-            'first_name' => 'default',
-            'last_name' => 'default',
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Str::random(10),
-            'rol_id' => 1
-        ]);
-        
-        $evento = Events::create([
-            'types_id' => $type->id,
-            'sessions_id' => $session->id,
-            'users_id' => $user->id,
-            'description' => 'default',
-            'date' => date("Y-m-d")
-        ]);
-
-        $evento2 = Events::create([
-            'types_id' => $type->id,
-            'sessions_id' => $session->id,
-            'users_id' => $user->id,
-            'description' => 'default',
-            'date' => date("Y-m-d")
-        ]);
 
     }
 }
