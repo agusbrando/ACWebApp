7df400d6a1a56ff0c6c6fdbde37b6adf1ff7e6fc
sefape <sefape@campusaula.com>
2020-04-07 10:23:09 +0200

    feat - modelo del timetable

---
+++ app/Models/Timetable.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timetable extends Model
+{
+    protected $table ='timetables';
+ 
+    protected $primaryKey ='id';
+ 
+    protected $fillable = [
+        'name'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany('App\Models\user');
+    }
+}
