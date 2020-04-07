e32360cfb8473cfd1101beb9e652c659d763cb2f
kehocu <kehocu@campusaula.com>
2020-04-07 13:58:59 +0200

    Deleted model comments

--- tests/Feature/TypeTest.php
+++ tests/Feature/TypeTest.php
@@ -20,54 +20,7 @@ class TypeTest extends TestCase
      * @return void
      */
     public function testEvents()
-    {
-        // $role = Role::create([
-        //     'name' => 'default',
-        //     'created_at' => now(),
-        //     'updated_at' => now()
-        // ]);
-        
-        // $type = Type::create([
-        //      'name' => 'default',
-        //      'model' => 'defaultModel'
-        //  ]);
-
-        // $classroom = Classroom::create([
-        //     'name' => 'default',
-        //     'number' => 1,
-        // ]);
-
-        // $session = Session::create([
-        //     'classroom_id'=>$classroom->id,
-        //     'time_start' => date('Y-m-d H:i:s'),
-        //     'time_end' => date('Y-m-d H:i:s'),
-        //     'model' => 'defaultModel'
-        // ]);
-
-        // $user = User::create([
-        //     'first_name' => 'default',
-        //     'last_name' => 'default',
-        //     'email' => 'default@gmail.com',
-        //     'password' => 'default',
-        //     'role_id' => $role->id
-        // ]);
-
-        //  $evento = Event::create([
-        //     'type_id' => $type->id,
-        //     'session_id' => $session->id,
-        //     'user_id' => $user->id,
-        //     'description' => 'default',
-        //     'date' => date("Y-m-d")
-        //  ]);
-
-        // $evento2 = Event::create([
-        //     'type_id' => $type->id,
-        //     'session_id' => $session->id,
-        //     'user_id' => $user->id,
-        //     'description' => 'default',
-        //     'date' => date("Y-m-d")
-        // ]);
-
+    {    
         $type = Type::find(1);
 
         $evento = $type->events->pluck('id');
@@ -77,13 +30,7 @@ class TypeTest extends TestCase
         ])->pluck('id');
 
         $this->assertEquals($evento,$expected_events_ids); 
-              
-        // $evento->delete();
-        // $user->delete();
-        // $session->delete();
-        // $classroom->delete();
-        // $type->delete();
-        // $role->delete();
+      
         }
 
     
