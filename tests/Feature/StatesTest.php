ca7fd65c8b0981606e7d21bf16694692bf976ff4
Sergio <serlomar@campusaula.com>
2020-04-08 15:06:18 +0200

    feat-Test States completado y funcionando

--- tests/Feature/StatesTest.php
+++ tests/Feature/StatesTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Carbon\Carbon;
 
+use App\Models\Item;
 use App\Models\State;
 
 class StatesTest extends TestCase
@@ -16,15 +17,45 @@ class StatesTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testItem()
     {
         $state = State::create([
             'name' => 'Roto',
             'created_at' => now(),
             'updated_at' => now()
         ]);
-        // $this->assertEquals($item->state_id, $state->id);
 
-        // $state->delete();
+        $item1 = Item::create([
+            'name' => 'Portatil Asus',
+            'date_pucharse' => Carbon::create('2020','03','30'),
+            'classroom_id' => 1,
+            'state_id' => $state->id,
+            'type_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $item2 = Item::create([
+            'name' => 'Portatil MSI',
+            'date_pucharse' => Carbon::create('2020','03','30'),
+            'classroom_id' => 1,
+            'state_id' => $state->id,
+            'type_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        $items = $state->items->pluck('id');
+        
+        $expected_items_ids = collect([
+            ['id' => $item1->id],
+            ['id' => $item2->id]
+        ])->pluck('id');
+
+        $this->assertEquals($items, $expected_items_ids);
+
+        $item2->delete();
+        $item1->delete();
+        $state->delete();
+        
     }
 }
