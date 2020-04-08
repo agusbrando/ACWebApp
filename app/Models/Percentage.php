91c09da2d1ffc0b01369e64d12b2f29c9388dba7
Javier Andres <javiamdazo@gmail.com>
2020-04-08 11:12:13 +0200

    Actualizacion modelos

--- app/Models/Percentage.php
+++ app/Models/Percentage.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Percentage extends Model
+class Percentage extends Pivot
 {
     protected $table = 'percentages';
     protected $guarded = [];
