839f233da6b97c3ae3ddc10fc59899dbc1a2ad7d
Ggarrido <guigarp1@gmail.com>
2020-04-08 15:38:42 +0200

    hotfix - delete error from merge all branches

--- database/migrations/2020_04_07_000026_create_posts_table.php
+++ database/migrations/2020_04_07_000026_create_posts_table.php
@@ -15,8 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->string('text');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
