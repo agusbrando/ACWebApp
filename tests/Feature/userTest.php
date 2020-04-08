05f38846284f2587ea31cff73ceb9eea41957ced
Sergio <serlomar@campusaula.com>
2020-04-08 12:36:34 +0200

    feat-Relacion entre Users e Items hecha

--- tests/Feature/userTest.php
+++ tests/Feature/userTest.php
@@ -15,8 +15,45 @@ class userTest extends TestCase
      */
     public function testExample()
     {
-        $response = $this->get('/');
+        $item = Item::create([
+            'name' => 'Portatil Asus',
+            'date_pucharse' => Carbon::create('2020','03','30'),
+            'classroom_id' => $classroom->id,
+            'state_id' => $state->id,
+            'type_id' => $type->id,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
 
-        $response->assertStatus(200);
+        $item2 = Item::create([
+            'name' => 'Portatil MSI',
+            'date_pucharse' => Carbon::create('2020','02','30'),
+            'classroom_id' => $classroom->id,
+            'state_id' => $state->id,
+            'type_id' => $type->id,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        
+        $this->assertEquals($item->classroom_id, $classroom->id);
+        $this->assertEquals($item->state_id, $state->id);
+        $this->assertEquals($item->type_id, $type->id);
+
+        //Creacion de la tabla intermedia con los datos extra que no son los id
+        $item->itemUser()->attach($itemuser, ['date_inicio' => Carbon::create('2019','09','16'),'date_fin' => Carbon::create('2020','06','12')]);
+        $item2->itemUser()->attach($itemuser, ['date_inicio' => Carbon::create('2019','04','16'),'date_fin' => Carbon::create('2020','06','12')]);
+        
+        //Array de Items recuperados
+        $items = $itemuser->items->pluck('id');
+
+        $expectedUsersIds = collect([
+            ['id'=> $item->id],
+            ['id'=> $item2->id]
+        ])->pluck('id');
+
+        $this->assertEquals($items, $expectedUsersIds);
+
+        $item->itemUser()->detach($itemuser);  
+        $item2->itemUser()->detach($itemuser); 
     }
 }
