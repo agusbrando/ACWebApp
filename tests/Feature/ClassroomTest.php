26bee8b25b23e0c4c3b701fdbbb830b6c7631541
carloscu <carloscu1234321@gmail.com>
2020-04-14 12:31:33 +0200

    no message

--- tests/Feature/ClassroomTest.php
+++ tests/Feature/ClassroomTest.php
@@ -14,7 +14,7 @@ use App\Models\Event;
 use Carbon\Carbon;
 
 use App\Models\Item;
-use App\Models\Classroom;
+
 class ClassroomTest extends TestCase
 {
     /**
@@ -85,7 +85,7 @@ class ClassroomTest extends TestCase
         $classroom->destroy($classroom);
         $type->destroy($type);
         $role->destroy($role);  
-
+    }
     public function testItem()
     {
         $classroom = Classroom::create([
