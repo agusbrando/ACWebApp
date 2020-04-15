34a6599ad0f7e040c5f3ba9a2c1acd908764d937
Javier Andres <javiamdazo@gmail.com>
2020-04-15 13:22:00 +0200

    Modificacion view NotasPorcentajes

--- app/Http/Controllers/NotasPorcentagesController.php
+++ app/Http/Controllers/NotasPorcentagesController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Subject;
 
 class NotasPorcentagesController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return view('NotesPercentages.index', compact('users'));
+        $subjects = Subject::all();
+        return view('NotesPercentages.index', compact('users', 'subjects'));
     }
 }
--- resources/views/NotesPercentages/index.blade.php
+++ resources/views/NotesPercentages/index.blade.php
@@ -4,11 +4,11 @@
 <script>
     $(document).ready(function() {
         $('#alumnos').DataTable( {
-            dom : "<'row'<'col-sm-6'l><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4'B><'col-sm-4'><'col-sm-4'p>>",
+            dom : "<'row'<'col-sm-6'l><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4 boton'B><'col-sm-4'><'col-sm-4'p>>",
             scrollY: 500,
             scrollCollapse: true,
             buttons:[
-                'excel'
+                { extend: 'excel', className: 'btn-outline-success' }
             ],
             language: {
                 "decimal": "",
@@ -37,21 +37,24 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="row">
     <div class="col-sm-12">
-    <div class="d-flex flex-md-nowrap">
+    <div class="d-flex flex-flow">
         <h1 class="display-4 pr-5">Notas y Porcentages</h1>
         <div class="dropdown">
             <button class=" btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Asignatura
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Dineño de Interfaces</a>
-                <a class="dropdown-item" href="#">Android</a>
-                <a class="dropdown-item" href="#">Ingles</a>
+                @foreach($subjects as $subject)
+                <a class="dropdown-item" href="#">{{$subject->name}}</a>
+                @endforeach
             </div>
         </div>  
+        <button class="btn btn-secondary evaluaciones" type="submit">
+            <a href="http://127.0.0.1:8000/Evaluaciones">Evaluaciones</a>
+        </button>
     </div>
         
-        <div class="d-flex flex-row bd-highlight mb-3">
+        <div class="d-flex flex-row bd-highlight mb-3 mt-n3">
             <div class="tablaPorcentajes porcentajes">
                 <table class="table table-striped mt-5">
                     <thead class="cabezeraTabla">
@@ -60,7 +63,7 @@
                             <td>Porcentaje</td>
                             <td>Nota Minima</td>
                             <td>Nota Media</td>
-                            <td colspan = 2>Actions</td>
+                            <td>Actions</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -70,16 +73,18 @@
                             <td>4,00</td>
                             <td>5,00</td>
                             <td>
-                                <a href="#" class="btn btn-primary">Edit</a>
-                            </td>
-                            <td>
                                 <form action="#" method="post">
-                                <button class="btn btn-danger" type="submit">Delete</button>
+                                <button class="btn btn-danger" type="submit">Borrar</button>
                                 </form>
                             </td>
                         </tr>
                     </tbody>
                 </table>
+                <form action="#" method="post">
+                    <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#exampleModal">
+                        Añadir
+                    </button>
+                </form>  
             </div> 
             <div class="tablaPorcentajes alumnos">
                 <table id="alumnos" class="table table-striped" style="width:100%">
@@ -111,3 +116,41 @@
     <div class="col-sm-12">
 </main>
 @endsection
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Añadir porcentaje</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form>
+        <div class="form-group">
+            <label for="Nombre">Nombre</label>
+            <input type="text" class="form-control" id="Nombre" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="Porcentaje">Porcentaje</label>
+            <input type="text" class="form-control" id="Porcentaje">
+        </div>
+        <div class="form-group">
+            <label for="nota_min">Nota Minima</label>
+            <input type="text" class="form-control" id="nota_min">
+        </div>
+        <div class="form-group">
+            <label for="nota_med">Nota Maxima</label>
+            <input type="text" class="form-control" id="nota_med">
+        </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Guardar Cambios</button>
+      </div>
+    </div>
+  </div>
+</div>
