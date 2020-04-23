78a538caa7c627fdd4c12a3ac08ac5b339fd5f34
Javier Andres <javiamdazo@gmail.com>
2020-04-23 13:26:03 +0200

    Modificacion

--- resources/views/Notas/editPorcentaje.blade.php
+++ resources/views/Notas/editPorcentaje.blade.php
@@ -19,7 +19,7 @@
             @csrf
             <div class="form-group">
                 <label for="Porcentaje">Porcentaje</label>
-                <select class="form-control" name="porcentaje" value={{ $porcentaje->percentage }}>
+                <select class="form-control" name="porcentaje" value={{$porcentaje->percentage}}>
                     <option value="0">0%</option>
                     <option value="10">10%</option>
                     <option value="20">20%</option>
