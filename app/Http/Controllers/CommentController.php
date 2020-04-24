4fd0323638a3b4f93b3e51a37a4105023a944922
adpesan <aperezsisagri@hotmail.com>
2020-04-24 02:42:42 +0200

    feat-controller comment

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\User;
+use App\Models\Post;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments = Comment::all();
+        return view('comments.index', compact('comments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('comments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'post_id' => 'required',
+            'text' => 'required'
+        ]);
+
+        $comment = new Comment([
+            'user_id' => $request->get('user_id'),
+            'post_id' => $request->get('post_id'),
+            'text' => $request->get('text')
+        ]);
+
+        $comment->save();
+        return redirect('/comments')->with('success', '¡Comentario guardado!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $comment = Comment::find($id);
+        $user = $comment->user;
+        $post = $comment->post;
+        return view('comments.show', compact('comment', 'user', 'post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $comment = Comment::find($id);
+        return view('comments.edit', compact('comment'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'post_id' => 'required',
+            'text' => 'required'
+        ]);
+
+        $comment = Comment::find($id);
+
+        $comment->user_id = $request->get('user_id');
+        $comment->post_id = $request->get('post_id');
+        $comment->text = $request->get('text');
+
+        $comment->save();
+        return redirect('/comments')->with('success', '¡Comentario actualizado!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        $comment->delete();
+
+        return redirect('/comments')->with('success', '¡Comentario borrado!');
+    }
+}
