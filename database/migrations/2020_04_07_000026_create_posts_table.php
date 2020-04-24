45a4957ef51c7cf8b5b9e5b976f09115f47eee27
adpesan <aperezsisagri@hotmail.com>
2020-04-24 02:20:50 +0200

    feat-migration posts

--- database/migrations/2020_04_07_000026_create_posts_table.php
+++ database/migrations/2020_04_07_000026_create_posts_table.php
@@ -16,10 +16,10 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('title');
             $table->string('text');
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
