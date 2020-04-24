e464516402fc552beca17fc88c7fd645f41cf503
adpesan <aperezsisagri@hotmail.com>
2020-04-24 02:22:59 +0200

    feat-migration comments

--- database/migrations/2020_04_07_000027_create_comments_table.php
+++ database/migrations/2020_04_07_000027_create_comments_table.php
@@ -16,10 +16,10 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->string('text');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('post_id')->unsigned();
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->string('text');
             $table->timestamps();
         });
     }
@@ -31,7 +31,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('comments');
     }
 }
