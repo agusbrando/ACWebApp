bc7733786e5df97e17daff179b235b64cc310af3
adpesan <aperezsisagri@hotmail.com>
2020-04-24 15:26:12 +0200

    feat-view items

---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,67 @@
+@extends('base')
+
+@section('main')
+
+<link href="{{ asset('css/units.css') }}" rel="stylesheet" type="text/css" />
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <h1 class="display-4">Crear Objeto </h1>
+    <hr>
+    <form method="post" action="{{ route('items.store') }}">   
+        <!-- Proteccion contra consultas no deseadas -->
+        {{ csrf_field() }} 
+
+        <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" class="form-control" id="nombreItem" name="name" aria-describedby="nombreHelp" placeholder="Nombre del objeto">
+        </div>
+        <div class="form-group">
+            <label for="nombre">Fecha de Compra</label>
+            <input type="date" id="date_pucharse" name="date_pucharse" placeholder="- Seleccionar fecha -" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="formControlSelect1">¿En que Aula va a estar?</label>
+            <select class="form-control" id="classroom_id" name="classroom_id">
+                <option disabled selected>Selecciona un Aula</option>
+                <!--Hace la funcion de un placeholder-->
+                @foreach($classrooms as $classroom)
+                <option value="{{$classroom->id}}">{{$classroom->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="formControlSelect1">Estado del objeto</label>
+            <select class="form-control" id="state_id" name="state_id">
+                <option  disable >Selecciona un estado</option>
+                <!--Hace la funcion de un placeholder-->
+                @foreach($states as $state)
+                @if($state->id == 1)
+                    <option selected value="{{$state->id}}">{{$state->name}}</option>
+                @else
+                    <option value="{{$state->id}}">{{$state->name}}</option>
+                
+                @endif
+                
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="formControlSelect1">Tipo de objeto</label>
+            <select class="form-control" id="type_id" name="type_id">
+                <option disabled selected>Selecciona un tipo</option>
+                <!--Hace la funcion de un placeholder-->
+                @foreach($types as $type)
+                <option value="{{$type->id}}">{{$type->name}}</option>
+                @endforeach
+            </select>
+        </div>
+
+
+        <div class="form-group">
+            <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button> -->
+            <a class="btn btn-primary" href="/items" role="button">Volver</a>
+            <button type="submit" class="btn btn-primary">Crear</button>
+        </div>
+    </form>
+
+</main>
+@endsection
---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,166 @@
+@extends('base')
+
+@section('main')
+
+<link href="{{ asset('css/units.css') }}" rel="stylesheet" type="text/css" />
+<script>
+        // <link href="{{ asset('resources/js/tabla.css') }}"  type="text/js" />
+        // $(document).ready(function() {
+        //     $('#datepicker').datepicker({
+        //         language: 'es'
+        //     });
+        // });
+        $(document).ready(function() {
+        $('#mytable').DataTable({
+            dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4 boton'B><'col-sm-4'><'col-sm-4'p>>",
+            scrollY: 500,
+            scrollCollapse: true, 
+            buttons: [{
+                    extend: 'excel',
+                    className: 'btn-outline-success mr-2'
+                },
+                {
+                    extend: 'pdf',
+                    className: 'btn-outline-danger mr-2'
+                }
+            ],
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de un total de _TOTAL_ Entradas",
+                "infoEmpty": "No hay informacion",
+                "infoFiltered": "(Filtrado de un total de _MAX_ entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "No se han encontrado resultados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
+        });
+    });
+    </script>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <!-- Listado de Items -->
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="display-4">Material del Aula </h1>
+            <form method="post" action="{{ url('items/filter') }}">
+
+                {{ csrf_field() }}
+
+                <div class="form-group float-left mr-3">
+                    <label for="formControlSelect1">Aulas</label>
+                    <select class="form-control " id="classroom_id" name="idClass">
+                        <option value="" selected>Todas</option>
+                        <!--Hace la funcion de un placeholder-->
+                        @foreach($classrooms as $classroom)
+                        <option value="{{$classroom->id}}">{{$classroom->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group float-left mr-3">
+                    <label for="formControlSelect1">Tipo</label>
+                    <select class="form-control" id="type_id" name="idType">
+                        <option value="" selected>Todos</option>
+                        <!--Hace la funcion de un placeholder-->
+                        @foreach($types as $type)
+                            <option value="{{$type->id}}">{{$type->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group float-left mr-3">
+                    <label for="formControlSelect1">Estado</label>
+                    <select class="form-control" id="state_id" name="idState">
+                        <option  value="" selected>Todos</option>
+                        <!--Hace la funcion de un placeholder-->
+                        @foreach($states as $state)
+                        <option value="{{$state->id}}">{{$state->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="d-flex flex-row">
+                    <button  class="btn btn-primary " type="submit" >Filtrar</button>
+                    <a class="btn btn-primary" href="/items/create" role="button">Añadir Material</a>
+                </div>
+                
+            </form>
+            
+            
+            <!-- Enlace al form create -->
+            <br>
+            <hr>
+
+            <div class="d-flex flex-row bd-highlight mb-3">
+                <div class="container-fluid">
+                    <table id='mytable' class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nombre</th>
+                                <th>Fecha Compra</th>
+                                <th>Id clase</th>
+                                <th>Estado</th>
+                                <th>Tipo</th>
+                                <th>Fecha creación</th>
+                                <th>Fecha actualización</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($items as $item)
+                            <tr>
+                                <td>{{$item->id}}</td>
+                                <td>{{$item->name}}</td>
+                                <td>{{$item->date_pucharse}}</td>
+                                @foreach($classrooms as $classroom)
+                                    @if($item->classroom_id == $classroom->id)
+                                        <td>{{$classroom->name}}</td>
+                                    @endif
+                                @endforeach
+                                @foreach($states as $state)
+                                    @if($item->state_id == $state->id)
+                                        <td>{{$state->name}}</td>
+                                    @endif
+                                @endforeach
+                                @foreach($types as $type)
+                                    @if($item->type_id == $type->id)
+                                        <td>{{$type->name}}</td>
+                                    @endif
+                                @endforeach
+                                <td>{{$item->created_at}}</td>
+                                <td>{{$item->updated_at}}</td>
+                                <td class="botones">
+                                    <a href="#" class="btn btn-primary">Edit</a>
+                                    <form method="post" action="{{ route('items.destroy', $item->id)}}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="submit">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    </div>
+    
+
+
+</main>
+@endsection
