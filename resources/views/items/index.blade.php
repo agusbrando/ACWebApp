d0b497b07cf5caf8d61dd947b4c8ea1dfbd98460
Sergio <serlomar@campusaula.com>
2020-04-27 11:15:31 +0200

    feat-Link al formulario editar hecho, sin funcionar

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -140,8 +140,13 @@
                                 <td>{{$item->created_at}}</td>
                                 <td>{{$item->updated_at}}</td>
                                 <td class="botones">
-                                    <a href="#" class="btn btn-primary">Edit</a>
+                                    <!-- <a href="{{ route('items.edit', $item->id)}}" class="btn btn-primary">Edit</a> -->
                                     <form method="post" action="{{ route('items.destroy', $item->id)}}">
+                                        @csrf
+                                        @method('GET')
+                                        <button class="btn btn-primary" type="submit">Edit</button>
+                                    </form>
+                                    <form method="post" action="{{ route('items.edit', $item->id)}}">
                                         @csrf
                                         @method('DELETE')
                                         <button class="btn btn-danger" type="submit">Delete</button>
