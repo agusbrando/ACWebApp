dd83c3a2f22dea4f8b70801d5416cd0f2db0ec5a
carloscu <carloscu1234321@gmail.com>
2020-04-27 16:55:02 +0200

    estructura inicial horarios/timetables

--- resources/views/Timetable/horario.blade.php
+++ resources/views/Timetable/horario.blade.php
@@ -7,7 +7,64 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     
                 <h1 class="display-3">Horario {{$timetable->name}} </h1>
-
+<table class="tabla">
+    <tr>
+        <th></th>
+        <th>LUNES</th>
+        <th>MARTES</th>
+        <th>MIÉRCOLES</th>
+        <th>JUEVES</th>
+        <th>VIERNES</th>
+    </tr>
+    <tr>
+       
+        <td class="td">8:30-9:25</td>
+        
+    </tr>
+    <tr>
+       
+        <td class="td">9:25-10:20</td>
+        
+    </tr>
+    <tr>
+       
+        <td class="td">10:20-10:40</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td>   
+    </tr>
+    <tr>
+       
+       <td class="td">10:40-11:35</td>
+       
+   </tr>
+   <tr>
+       
+       <td class="td">11:35-12:25</td>
+       
+   </tr>
+   <tr>
+       
+       <td class="td">12:25-12:40</td>
+       <td>DESCANSO</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td>
+        <td>DESCANSO</td> 
+   </tr>
+   <tr>
+       
+       <td class="td">12:40-13:35</td>
+       
+   </tr>
+   <tr>
+       
+       <td class="td">13:35-14:30</td>
+       
+   </tr>
+</table>
 
 
             
