d222642937b28de7cc7d6756dcc692b72eb971c3
carloscu <carloscu1234321@gmail.com>
2020-04-28 11:50:00 +0200

    firma se "guarda" y calendario cambiado por tabla

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Tracking;
+use Dotenv\Store\File\Paths;
 use Illuminate\Http\Request;
 
 class TrackingController extends Controller
@@ -11,7 +12,9 @@ class TrackingController extends Controller
   
   public function index()
   {
-    return view('Tracking.index');
+    $trackings=Tracking::all();
+        return view('Tracking.index',compact('trackings'));
+    
   }
 
   
@@ -28,7 +31,20 @@ class TrackingController extends Controller
           'firma'=>'required'
         ]);
         
-  
+      
+	  
+        $image_parts = explode(";base64,", $request->get('firma'));
+            
+        $image_type_aux = explode("image/", $image_parts[0]);
+          
+        $image_type = $image_type_aux[1];
+          
+        $image_base64 = base64_decode($image_parts[1]);
+          
+        $file = "upload\'".uniqid() . '.'.$image_type;
+        //$file->move(storage_path("app/tracking/$tracking->id"));
+      
+        file_put_contents($file, $image_base64);
         
            
         
@@ -37,12 +53,12 @@ class TrackingController extends Controller
         list($hora_fin_1,$hora_fin_2) = explode(":",$request->get('time_end'));
         list($hora_ini_3,$hora_ini_4) = explode(":",$request->get('time_start2'));
         list($hora_fin_3,$hora_fin_4) = explode(":",$request->get('time_end2'));
-        $suma=(($hora_fin_1-$hora_ini_1)*60)+($hora_fin_2-$hora_ini_2)+(($hora_fin_3-$hora_ini_3)*60)+($hora_fin_4-$hora_ini_4);
+        $suma=(($hora_fin_1-$hora_ini_1)*60)+($hora_fin_2-$hora_ini_2)+((($hora_fin_3-$hora_ini_3)*60)+($hora_fin_4-$hora_ini_4));
         
         $user = Auth::user();
         $num_hours=$suma/60;
         $tracking = new Tracking([
-            'signature'=>$request->get('firma'),
+            'signature'=>$file,
             'user_id' => $user->id,
             'datetime_start' =>$request->get('date_start'),
             'datetime_end' =>$request->get('date_start'),
--- resources/views/Tracking/index.blade.php
+++ resources/views/Tracking/index.blade.php
@@ -1,20 +1,48 @@
 @extends('base')
 
 @section('main')
-
+<script>
+    $(document).ready(function() {
+        $('#tabla').DataTable({
+            dom: "<'row'<'col-sm-6'l><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-4 boton'B><'col-sm-4'><'col-sm-4'p>>",
+            buttons: [{
+                    extend: 'excel',
+                    className: 'btn-outline-success mr-2'
+                },
+                {
+                    extend: 'pdf',
+                    className: 'btn-outline-danger mr-2'
+                }
+            ],
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de un total de _TOTAL_ Entradas",
+                "infoEmpty": "No hay informacion",
+                "infoFiltered": "(Filtrado de un total de _MAX_ entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "No se han encontrado resultados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
+        });
+    });
+</script>
 
 
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script type="text/javascript" src="http://keith-wood.name/js/jquery.signature.js"></script>
-<script type="text/javascript">
-  $(function() {
-    $('#datetimepicker13').datetimepicker({
-      inline: true,
-      sideBySide: false
-    });
-  });
-</script>
+
 
 
 <link href="{{ asset('css/seguimiento.css') }}" rel="stylesheet" type="text/css" />
@@ -85,16 +113,32 @@
 
 
     <div class="row border-right-0 border-top-0 border-bottom-0 col-12 col-md-6 calendario w-100">
-      <div class="col-12 p-0" style="overflow:hidden;">
-        <div class="form-group">
-          <div class="row">
-            <div class="col-12">
-              <div id="datetimepicker13"></div>
-            </div>
-          </div>
-        </div>
+      
+            <table class="table table-striped" id="tabla">
+                        <thead class="cabezeraTabla">
+                            <tr>
+                                <td>Fecha Firma</td>
+                                <td>Hora Inicio</td>
+                                <td>Hora Final</td>
+                                <td>Suma Horas</td>
+                                <td>Firma</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($trackings as $tracking)
+                            <tr>
+                                <td>{{$tracking->datetime_start}}</td>
+                                <td>8:30</td>
+                                <td>14:30</td>
+                                <td>{{$tracking->num_hours}}</td>
+                                <td>{{$tracking->signature}}</td>
+                            </tr>
+                            @endforeach
+                            
+                        </tbody>
 
-      </div>
+                    </table>
+                                              
     </div>
 
   </div>
