379fab5e0aeadc07d6ab368aade0c96bc8fd5d1e
sefape <sefape@campusaula.com>
2020-04-28 17:34:27 +0200

    feat - card para los archivos subidos que se puedan descargar

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,14 +1,30 @@
 @extends('base')
 @section('main')
-
+<link href="{{ asset('css/messages.css') }}" rel="stylesheet" type="text/css" />
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="row">
         <div class="col-sm-12">
             <div class="card">
                 <h5 class="card-header">{{$message->subject}}</h5>
                 <div class="card-body">
-                    <h5 class="card-title">{{$message->user_id}}</h5>
+                    <h5 class="card-title">{{$message->user->first_name}} {{$message->user->last_name}}</h5>
                     <p class="card-text">{{$message->message}}</p>
+                <div class="containerAttachment ">
+                    @foreach($message->attachments as $attachment)
+
+                    <div class="card m-2" style="width: 6rem;">
+                        <img class="card-img-top" src="/img/iconfiles.png" alt="Card image cap">
+                        <div class="card-body">
+                          <p class="card-text">
+                            <a href="{{ url('download_attachment_message',array($message->id,$attachment->name))}}">
+                            {{$attachment->name}}
+                        </a>
+                    </p>
+                        </div>
+                      </div>
+
+                @endforeach
+            </div>
                 </div>
             </div>
         </div>
