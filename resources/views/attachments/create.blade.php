a5c3b76fb134b6aadaf016387c902aa99495125e
adpesan <aperezsisagri@hotmail.com>
2020-04-28 03:20:18 +0200

    fix-view controller

--- resources/views/attachments/create.blade.php
+++ resources/views/attachments/create.blade.php
@@ -3,37 +3,58 @@
 @section('main')
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
     <div class="row">
-        <div class="col-sm-8 offset-sm-2">
-            <h1 class="display-3">Añadir Attachment</h1>
-            <div>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    <br/>
-                @endif
-                <form method="post" action="{{ route('attachments.store') }}">
-                    @csrf
-                        <div class="form-group">
-                            <label for="name">Nombre:</label>
-                            <input type="text" class="form-control" name="name"/>
+        <div class="col-sm-12">
+            <h1 class="display-3">Attachments</h1>
+            <body>
+                <div class="container lst">
+                    @if(count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>¡Lástima!</strong> Ha habido algún que otro problema.<br><br>
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li>{{$error}}</li>
+                                @endforeach
+                            </ul>
                         </div>
-                        <div class="form-group">
-                            <label for="attachmentable_id">Id Attachmentable:</label>
-                            <input type="number" class="form-control" name="attachmentable_id"/>
+                    @endif
+                    @if(session('success'))
+                        <div class="alert alert-success">
+                            {{session('success')}}
                         </div>
-                        <div class="form-group">
-                            <label for="attachmentable_type">Attachmentable Type:</label>
-                            <input type="text" class="form-control" name="attachmentable_type"/>
+                    @endif
+                    <form method="post" action="{{url('attachment')}}" enctype="multipart/form-data">
+                        {{csrf_field()}}
+                        <div class="input-group hdtuto control-group lst increment">
+                            <input type="file" name="name[]" class="myfrm form-control">
+                            <div class="input-group-btn">
+                                <button class="btn btn-success" type="button"><i class="fldemo glyphicon glyphicon-plus"></i>Añadir</button>
+                            </div>
                         </div>
-                        <button type="submit" class="btn btn-primary-outline">Añadir Attachment</button>
-                </form>
-            </div>
+                        <div class="clone hide">
+                            <div class="hdtuto control-group lst input-group" style="margin-top:10px">
+                                <input type="file" name="name[]" class="myfrm form-control">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-danger" type="button"><i class="fldemo glyphicon glyphicon-remove"></i>Eliminar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-success" style="margin-top:10px">Subir</button>
+                    </form>
+                </div>
+            </body>
         </div>
     </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".btn-success").click(function() {
+                var lsthmtl = $(".clone").html();
+                $(".increment").after(lsthmtl);
+            });
+
+            $("body").on("click",".btn-danger",function() {
+                $(this).parents(".hdtuto control-group lst").remove();
+            });
+        });
+    </script>
 </main>
 @endsection
