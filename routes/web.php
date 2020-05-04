460246ec2b9c8140d1b55d1d2575b425ccdfb709
sefape <sefape@campusaula.com>
2020-05-04 20:13:30 +0200

    feat - adicion de la ruta para el metodo de visto

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,7 @@ Route::get('messages_send', 'MessageController@index')->middleware('auth')->name
 
 Route::get('download_attachment_message/{idm}/{nameAttach}','MessageController@download')->name('downloadmessagefile');
 
+Route::get('sended/{id}', 'MessageController@showSended')->middleware('auth');
+
 Route::get('/', 'HomeController@index');
 
