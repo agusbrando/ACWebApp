dcbcedba784c4fe629d164054672c8bfae3dacc5
Sergio <serlomar@campusaula.com>
2020-05-04 12:26:49 +0200

    feat-ItemYear

---
+++ database/seeds/ItemYearTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ItemYearTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('itemYear')->insert([
+            'item_id' => '1',
+            'year_union_user_id' => '1',
+            'name' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('itemYear')->insert([
+            'item_id' => '2',
+            'year_union_user_id' => '2',
+            'name' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('itemYear')->insert([
+            'item_id' => '3',
+            'year_union_user_id' => '3',
+            'name' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('itemYear')->insert([
+            'item_id' => '1',
+            'year_union_user_id' => '2',
+            'name' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('itemYear')->insert([
+            'item_id' => '2',
+            'year_union_user_id' => '1',
+            'name' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
