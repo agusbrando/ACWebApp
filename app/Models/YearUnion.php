cb2cc8de1fb77feebbb04a79818f45ab6922fcfe
Sergio <serlomar@campusaula.com>
2020-05-04 12:47:14 +0200

    ref-YearUnion

--- app/Models/YearUnion.php
+++ app/Models/YearUnion.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class YearUnion extends Model
 {
-    //
+    protected $table = 'yearUnions';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = true;
+    
+    protected $guarded =[];
 }
