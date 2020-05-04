77c85d06c7b0d2a30dcd403fd5fa949bca9836a9
Sergio <serlomar@campusaula.com>
2020-05-04 12:36:41 +0200

    feat-YearUnionUser table seeder creado

---
+++ database/seeds/YearUnionUserTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class YearUnionUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('yearUnionUser')->insert([
+            'year_union_id' => '2',
+            'user_id' => '2',
+            'assistance' => 'true',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('yearUnionUser')->insert([
+            'year_union_id' => '1',
+            'user_id' => '1',
+            'assistance' => 'true',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('yearUnionUser')->insert([
+            'year_union_id' => '3',
+            'user_id' => '3',
+            'assistance' => 'true',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('yearUnionUser')->insert([
+            'year_union_id' => '6',
+            'user_id' => '2',
+            'assistance' => 'true',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('yearUnionUser')->insert([
+            'year_union_id' => '5',
+            'user_id' => '3',
+            'assistance' => 'true',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
