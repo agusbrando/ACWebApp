2c9937f0bbdb7a1e67f556b667fe684699645aeb
sefape <sefape@campusaula.com>
2020-05-04 20:13:47 +0200

    wip - mensajes

---
+++ app/Mail/Message.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class message extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //TODO change to gobal variable
+        return $this->from('sefape@campusaula.com')
+        ->view('emails/message');
+    }
+}
