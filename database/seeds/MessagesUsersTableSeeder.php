b32129981bd0a96129c07d1d70585bab148ca5a0
sefape <sefape@campusaula.com>
2020-05-04 20:11:26 +0200

    fix - correccion en los seeders

--- database/seeds/MessagesUsersTableSeeder.php
+++ database/seeds/MessagesUsersTableSeeder.php
@@ -11,34 +11,33 @@ class MessagesUsersTableSeeder extends Seeder
      */
     public function run()
     {
-            DB::table('message_user')->insert([
-                'message_id'         =>     1,
-                'user_id'         =>    2,
-                'created_at' => now(),
-                'updated_at' => now(),
-                'read'         =>    false
-            ]);
-            DB::table('message_user')->insert([
-                'message_id'         =>     2,
-                'user_id'         =>    3,
-                'created_at' => now(),
-                'updated_at' => now(),
-                'read'         =>    false
-            ]);
-            DB::table('message_user')->insert([
-                'message_id'         =>     3,
-                'user_id'         =>    4,
-                'created_at' => now(),
-                'updated_at' => now(),
-                'read'         =>    false
-            ]);
-            DB::table('message_user')->insert([
-                'message_id'         =>     4,
-                'user_id'         =>    1,
-                'created_at' => now(),
-                'updated_at' => now(),
-                'read'         =>    false
-            ]);
-
+        DB::table('message_user')->insert([
+            'message_id' => 1,
+            'user_id' => 2,
+            'read' => '0',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('message_user')->insert([
+            'message_id' => 2,
+            'user_id' => 3,
+            'read' => '0',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('message_user')->insert([
+            'message_id' => 3,
+            'user_id' => 1,
+            'read' => '0',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('message_user')->insert([
+            'message_id' => 4,
+            'user_id' => 1,
+            'read' => '0',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
