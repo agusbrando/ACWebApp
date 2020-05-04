eca76d79c1e9ac38ed73359a46940a4b73bdd02e
jegagi <jegagi32@gmail.com>
2020-05-04 11:47:34 +0200

    feat: añado year union table

---
+++ database/migrations/2020_04_07_000032_create_year_union_table copy.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class YearUnionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('yearUnions', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->integer('subject_id')->unsigned();
+            $table->integer('course_id')->unsigned();
+            $table->integer('evaluation_id')->unsigned();
+            $table->integer('year_id')->unsigned();
+            $table->integer('program_id')->unsigned();
+            $table->integer('responsable_id')->unsigned();
+            $table->string('notes');
+            $table->string('date_check');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->timestamps();
+            $table->foreign('subject_id')->references('id')->on('subjects');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('evaluation_id')->references('id')->on('evaluations');
+            $table->foreign('year_id')->references('id')->on('years');
+            $table->foreign('program_id')->references('id')->on('programs');
+            $table->foreign('responsable_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('yearUnions');
+    }
+}
