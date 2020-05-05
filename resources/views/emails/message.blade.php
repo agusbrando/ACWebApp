fedae29282662c7f1fb3f0d9b12b8fc052b5b797
sefape <sefape@campusaula.com>
2020-05-05 19:16:49 +0200

    feat - vista del mensaje que se manda por correo

---
+++ resources/views/emails/message.blade.php
@@ -0,0 +1,14 @@
+<body>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="display-4 mb-0">Mensajes</h1>
+        </div>
+        <p>Tienes un nuevo mensaje disponible en la plataforma. </p>
+        <p>Asunto: {{ $messageSubject }}</p>
+        <p>Presiona el siguiente boton para acceder a ella:</p>
+
+    </div>
+
+
+</body>
