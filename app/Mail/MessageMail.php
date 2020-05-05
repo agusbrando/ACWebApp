2cb5e66fb93cd52de9dade453314d703b51e51be
sefape <sefape@campusaula.com>
2020-05-05 19:16:35 +0200

    feat - codigo que manda el mensaje por correo

---
+++ app/Mail/MessageMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class messageMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    protected $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //TODO change to gobal variable
+        return $this->view('emails/message')->with([
+            'messageSubject' => $this->message->subject,
+
+        ]);
+    }
+}
