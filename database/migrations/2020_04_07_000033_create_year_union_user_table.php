7232589a79bfb9f21fc49abc74e49c25292c0a25
Sergio <serlomar@campusaula.com>
2020-05-05 12:41:18 +0200

    fix-Migrations year

--- database/migrations/2020_04_07_000033_create_year_union_user_table.php
+++ database/migrations/2020_04_07_000033_create_year_union_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateYearUnionUsersTable extends Migration
+class CreateYearUnionUserTable extends Migration
 {
     /**
      * Run the migrations.
--- database/seeds/YearUnionUserTableSeeder.php
+++ database/seeds/YearUnionUserTableSeeder.php
@@ -11,35 +11,35 @@ class YearUnionUserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('yearUnionUser')->insert([
+        DB::table('yearUnionUsers')->insert([
             'year_union_id' => '2',
             'user_id' => '2',
             'assistance' => 'true',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-        DB::table('yearUnionUser')->insert([
+        DB::table('yearUnionUsers')->insert([
             'year_union_id' => '1',
             'user_id' => '1',
             'assistance' => 'true',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-        DB::table('yearUnionUser')->insert([
+        DB::table('yearUnionUsers')->insert([
             'year_union_id' => '3',
             'user_id' => '3',
             'assistance' => 'true',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-        DB::table('yearUnionUser')->insert([
+        DB::table('yearUnionUsers')->insert([
             'year_union_id' => '6',
             'user_id' => '2',
             'assistance' => 'true',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-        DB::table('yearUnionUser')->insert([
+        DB::table('yearUnionUsers')->insert([
             'year_union_id' => '5',
             'user_id' => '3',
             'assistance' => 'true',
