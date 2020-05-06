2212aef8a76df898d16f59ca16b8e441d9a0de75
Sergio <serlomar@campusaula.com>
2020-05-06 13:59:58 +0200

    feat-Listar stados hecho

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\State;
 
 class StateController extends Controller
 {
@@ -13,7 +14,9 @@ class StateController extends Controller
      */
     public function index()
     {
-        //
+        
+        $states = State::all();
+        return view('states.index', compact('states'));
     }
 
     /**
