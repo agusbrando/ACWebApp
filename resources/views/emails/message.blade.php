c8f42dc063b486e7f87fbb5f36eeec218cd6a61a
sefape <sefape@campusaula.com>
2020-05-07 19:06:51 +0200

    feat - adecuacion del blade al proyecto

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -2,12 +2,12 @@
 
     <div class="row">
         <div class="col-sm-12">
-            <h1 class="display-4 mb-0">Mensajes</h1>
+            <h1 class="display-4 mb-0">AULACAMPUS</h1>
         </div>
         <p>Tienes un nuevo mensaje disponible en la plataforma. </p>
         <p>Asunto: {{ $messageSubject }}</p>
         <p>Presiona el siguiente boton para acceder a ella:</p>
-
+<a href="{{$url}}" class="btn btn-primary mt-3">Accede aquí</a>
     </div>
 
 
