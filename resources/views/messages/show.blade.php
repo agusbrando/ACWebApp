e6d3a9508193203f1278d6f5c9d47b61df51c9fc
sefape <sefape@campusaula.com>
2020-05-07 19:08:00 +0200

    feat - adicion del estilo conjunto al proyecto

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -2,37 +2,60 @@
 @section('main')
 <link href="{{ asset('css/messages.css') }}" rel="stylesheet" type="text/css" />
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="card">
-                <h5 class="card-header">{{$message->subject}}</h5>
-                <div class="card-body">
-                    <h5 class="card-title">{{$message->user->first_name}} {{$message->user->last_name}}</h5>
-                    <p class="card-text">{{$message->message}}</p>
-                <div class="containerAttachment ">
-                    @foreach($message->attachments as $attachment)
-
-                    <div class="card m-2" style="width: 6rem;">
-                        <img class="card-img-top" src="/img/iconfiles.png" alt="Card image cap">
-                        <div class="card-body">
-                          <p class="card-text">
-                            <a href="{{ url('download_attachment_message',array($message->id,$attachment->name))}}">
-                            {{$attachment->name}}
-                        </a>
-                    </p>
-                        </div>
-                      </div>
-
-                @endforeach
-            </div>
-                </div>
+    <div class="card shadow">
+        <div class="card-header row m-0 justify-content-between">
+            @if ($sitio == 0)
+                <h3>Mensaje recibido</h3>
+            @else
+                <h3>Mensaje enviado</h3>
+            @endif
+            <div>
+                @if ($sitio == 0)
+                    <a class="btn btn-outline-success" href="">Responder</a>
+                @endif
+                @if ($sitio == 0)
+                    <a class="btn btn-outline-info" href="{{ url('messages')}}">Volver</a>
+                @else
+                    <a class="btn btn-outline-info" href="{{ url('messages_send')}}">Volver</a>
+                @endif
+                <form class="float-right"action="{{ route('messages.destroy',$message->id)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-outline-danger ml-1">Eliminar</button>
+                </form>
             </div>
         </div>
+        <div class="card-body row no-gutters">
+            <div class="col-12 col-md-5 col-lg-3 p-3">
+                <div>
+                    <h5 class="card-title">{{$message->subject}}</h5>
+                    <p class="card-text">FROM: {{$message->user->first_name}} {{$message->user->last_name}}</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-7 col-lg-9 p-3" style="border: 1px solid gray">
+                <p>{{$message->message}}</p>
+            </div>
     </div>
-    @if ($sitio == 0)
-    <a href="{{ url('messages')}}" class="btn btn-primary m-3">Volver</a>
-    @else
-    <a href="{{ url('messages_send')}}" class="btn btn-primary m-3">Volver</a>
-    @endif
+
+@if ($message->attachments->count() != 0)
+<div class=" card-footer col-12">
+    <div class="m-4">
+        <h3>Adjuntos</h3>
+        @foreach($message->attachments as $attachment)
+        <div class="card-body row no-gutters">
+            <div class="col-12 col-md-3 col-lg-1 p-3">
+                <img class="img-thumbnail" src="/img/iconfiles.png" alt="">
+            </div>
+            <div class="col-12 col-md-8 col-lg-10 p-3">
+                <div>
+                 <a class="btn btn-outline-info" href="{{url('download_attachment_message',array($message->id,$attachment->name))}}"><h5 class="card-title">{{$attachment->name}}</h5></a>
+                 </div>
+             </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endif
+</div>
 </main>
 @endsection
