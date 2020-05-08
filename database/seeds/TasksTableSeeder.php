e2e75c552fb81fa78b4c5ee779b2f5fcdc93a672
Javier Andres <javiamdazo@gmail.com>
2020-05-08 09:50:18 +0200

    Arreglo tabla

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -11,41 +11,6 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('tasks')->insert([
-            'evaluation_id' => 1,
-            'type_id' => 9,
-            'name' => 'Practica 1',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('tasks')->insert([
-            'evaluation_id' => 1,
-            'type_id' => 8,
-            'name' => 'Parcial 1',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('tasks')->insert([
-            'evaluation_id' => 1,
-            'type_id' => 8,
-            'name' => 'Parcial 2',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('tasks')->insert([
-            'evaluation_id' => 1,
-            'type_id' => 8,
-            'name' => 'Parcial 3',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        DB::table('tasks')->insert([
-            'evaluation_id' => 1,
-            'type_id' => 9,
-            'name' => 'Practica 2',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
 
     }
 }
