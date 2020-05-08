44ac937653c4a181dea18df04391f73fab683359
adpesan <aperezsisagri@hotmail.com>
2020-05-08 03:35:39 +0200

    fix-view comments

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -1,37 +1,72 @@
 @extends('base')
+
 @section('main')
+
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-    <div class="row">
-        <div class="col-sm-8 offset-sm-2">
-            <h1 class="display-3">Actualizar Comentario</h1>
-            @if($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                <br/>
-            @endif
-            <form method="post" action="{{ route('comments.update', $comment->id) }}">
+    <link href="{{ asset('css/posts.css') }}" rel="stylesheet" type="text/css" />
+
+    <div class="card shadow">
+        <div class="card-header row m-0 justify-content-between">
+            <h3>Editar Comentario</h3>
+            <form action="{{ route('comments.update',$post->id)}}" method="POST">
                 @method('PATCH')
-                    @csrf
+                <div>
+                    <div class="col-12">
+                        <input class="btn btn-outline-success float-right ml-1" type='submit' value="Guardar">
+                        <a class="btn btn-outline-warning float-right" href="{{ route('comments.show',$comment->id)}}" tabindex="-1" aria-disabled="true">Cancelar</a>
+                    </div>
+                </div>
+        </div>
+        <div class="card-body row no-gutters">
+            <div class="col-12 col-md-4 col-lg-2 p-3">
+                <img src="{{asset('img/default_post.jpg')}}" class="img-thumbnail" alt="...">
+            </div>
+            <div class="col-12 col-md-8 col-lg-10 p-3">
+                <div>
+
+                    <fieldset>
                         <div class="form-group">
                             <label for="user_id">Id Usuario</label>
-                            <input type="number" class="form-control" name="user_id" value="{{ $comment->user_id }}"/>
+                            <input value="{{$comment->user_id}}" name="user_id" id="user_id" type="number" class="@error('user_id') is-invalid @enderror form-control">
                         </div>
                         <div class="form-group">
                             <label for="post_id">Id Post</label>
-                            <input type="number" class="form-control" name="post_id" value="{{ $comment->post_id }}"/>
+                            <input value="{{$comment->title}}" name="post_id" id="post_id" type="number" class="@error('post_id') is-invalid @enderror form-control">
                         </div>
                         <div class="form-group">
                             <label for="text">Texto</label>
-                            <input type="text" class="form-control" name="text" value="{{ $comment->text }}"/>
+                            <input value="{{$comment->text}}" name="text" id="text" type="text" class="@error('text') is-invalid @enderror form-control">
                         </div>
-                        <button type="submit" class="btn btn-primary">Actualizar</button>
-            </form>
+                    </fieldset>
+                    @error('email', 'login')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
         </div>
+
+        </form>
     </div>
 </main>
 @endsection
+
+
+<!-- Rutas -->
+
+<!-- Route::resource('users','UserController');
+Route::get('users/edit/{id}',['as' => 'users.showedit', 'uses' => 'UserController@show']); -->
+
+<!-- Controller -->
+
+<!-- public function show($user_id)
+    {
+        $user = User::find($user_id);
+        $edit = false;
+        if(URL::current() == url("/users/edit/".$user_id)){
+            $edit = true;
+        }
+        return view('users.show', compact('user','edit'));
+    } -->
+<!-- Import de URL -->
+<!-- use Illuminate\Support\Facades\URL; -->
+<!--  -->
