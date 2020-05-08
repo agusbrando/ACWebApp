d1075943cdce5da32ecba1fda11cedfb11ad0a6f
jegagi <jegagi32@gmail.com>
2020-05-08 11:56:17 +0200

    fix: añado seeder comentados de prueba

--- database/seeds/YearUnionsTableSeeder.php
+++ database/seeds/YearUnionsTableSeeder.php
@@ -67,19 +67,28 @@ class YearUnionsTableSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
-        DB::table('yearUnions')->insert([
-            'subject_id' => '1',
-            'course_id' => '4',
-            'evaluation_id' => '2',
-            'year_id' => '3',
-            'program_id' => '1',
-            'responsable_id' => '4',
-            'date_check' => '2019-09-22',
-            'date_start'=> '2019-09-22',
-            'date_end'=> '2019-12-01',		
-            'notes' => 'Muy bien estructurado',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+
+        /*
+            for($i = 1; $i<=3; $i++){
+
+                DB::table('yearUnions')->insert([
+                    'subject_id' => '1',
+                    'course_id' => '4',
+                    'evaluation_id' => $i,
+                    'year_id' => '3',
+                    'date_start'=> '2019-09-22',
+                    'date_end'=> '2019-12-01',		
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+        
+
+            }
+        */
+
+        
+
+
+
     }
 }
