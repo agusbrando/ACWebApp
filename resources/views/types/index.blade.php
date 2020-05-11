8168b69c36a6e8f9b4341006cbd6574cefbfcc44
kehocu <kehocu@campusaula.com>
2020-05-11 14:38:02 +0200

    Fix type file

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -5,7 +5,7 @@
     <div class="card shadow">
         <div class="card-header row m-0 justify-content-between">
         <div class="d-flex flex-row">
-                <a href="/types" class="my-auto mx-1 h5"><i class="fas fa-arrow-left"></i></a>
+                <a href="/events" class="my-auto mx-1 h5"><i class="fas fa-arrow-left"></i></a>
                 <h3> Tipos</h3>
             </div>
             <div>
