ff3ec09d2c742dc5684fb632f12184f2e4a96a43
sefape <sefape@campusaula.com>
2020-05-12 18:10:27 +0200

    feat - modificaciones del estilo del email

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -1,14 +1,11 @@
 <body>
-
-    <div class="row">
-        <div class="col-sm-12">
-            <h1 class="display-4 mb-0">AULACAMPUS</h1>
+    <div>
+        <div>
+            <h1>AULACAMPUS</h1>
         </div>
         <p>Tienes un nuevo mensaje disponible en la plataforma. </p>
         <p>Asunto: {{ $messageSubject }}</p>
         <p>Presiona el siguiente boton para acceder a ella:</p>
-<a href="{{$url}}" class="btn btn-primary mt-3">Accede aquí</a>
+        <a href="{{$url}}">Accede aquí</a>
     </div>
-
-
 </body>
