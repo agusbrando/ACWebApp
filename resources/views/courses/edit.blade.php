00b67285a74818134f72f152236497d6306a93b8
Sergio <serlomar@campusaula.com>
2020-05-14 10:57:44 +0200

    feat-ruta automatica para ir atrás anadido

--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -12,7 +12,7 @@
                 <div class="d-flex flex-row">
                     @csrf
                     @method('GET')
-                    <a href="{{ route('courses.index')}}" class="my-1 mx-1 h5"><i class="fas fa-arrow-left"></i></a>
+                    <a href="{{ url()->previous() }}" class="my-1 mx-1 h5"><i class="fas fa-arrow-left"></i></a>
 
                     <h3>Crear Curso</h3>
                 </div>
