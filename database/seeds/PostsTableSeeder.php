804dc31f3dedda25c4225099fb5ce3ac74e1586f
adpesan <aperezsisagri@hotmail.com>
2020-05-20 07:34:00 +0200

    fix-seeders adpesan

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,5 +12,21 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //TODO Create 2 post
+
+        DB::table('posts')->insert([
+            'user_id' => 1,
+            'title' => 'Título del Post 2',
+            'text' => 'Descripción del post 1',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('posts')->insert([
+            'user_id' => 2,
+            'title' => 'Título del Post 2',
+            'text' => 'Descripción del post 2',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
