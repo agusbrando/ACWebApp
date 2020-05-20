25d80be672ea32857493a6fa3131bb0718517beb
kehocu <kehocu@campusaula.com>
2020-05-20 13:40:59 +0200

    Modified CoursesSeeder

--- database/seeds/CoursesTableSeeder.php
+++ database/seeds/CoursesTableSeeder.php
@@ -29,6 +29,32 @@ class CoursesTableSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+        DB::table('courses')->insert([
+            'level' => 2,
+            'name' => 'Administracion de Sistemas Informaticos y en Red',
+            'abbreviation' =>'ASIR',
+            'num_students' => 23,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('courses')->insert([
+            'level' => 1,
+            'name' => 'FP Básica en Informática y Comunicaciones',
+            'abbreviation' =>'FPBASICA',
+            'num_students' => 30,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('courses')->insert([
+            'level' => 2,
+            'name' => 'FP Básica en Informática y Comunicaciones',
+            'abbreviation' =>'FPBASICA',
+            'num_students' => 30,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
 
         DB::table('courses')->insert([
             'level' => 1,
@@ -73,32 +99,7 @@ class CoursesTableSeeder extends Seeder
             'updated_at' => now()
         ]);
 
-        DB::table('courses')->insert([
-            'level' => 2,
-            'name' => 'Administracion de Sistemas Informaticos y en Red',
-            'abbreviation' =>'ASIR',
-            'num_students' => 23,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        DB::table('courses')->insert([
-            'level' => 1,
-            'name' => 'FP Básica en Informática y Comunicaciones',
-            'abbreviation' =>'FPBASICA',
-            'num_students' => 30,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        DB::table('courses')->insert([
-            'level' => 2,
-            'name' => 'FP Básica en Informática y Comunicaciones',
-            'abbreviation' =>'FPBASICA',
-            'num_students' => 30,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+       
 
 
     }
