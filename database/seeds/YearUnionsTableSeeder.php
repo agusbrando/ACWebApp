c7bfce392c094ec5eb57c109a13ea188af000493
jegagi <jegagi32@gmail.com>
2020-05-21 10:03:05 +0200

    fix: arreglo error que no creaba las tres evaluaciones correctamente
    Signed-off-by: jegagi <jegagi32@gmail.com>

--- database/seeds/YearUnionsTableSeeder.php
+++ database/seeds/YearUnionsTableSeeder.php
@@ -24,26 +24,28 @@ class YearUnionsTableSeeder extends Seeder
             array_push($fechasInicioFin,['date_start'=> '2019-09-09', 'date_end'=>'2019-11-21']);//1ºEVAL
             array_push($fechasInicioFin,['date_start'=>'2019-11-22', 'date_end'=>'2020-02-24']);//2ºEVAL
             array_push($fechasInicioFin,['date_start'=>'2020-02-25', 'date_end'=>'2020-05-28']);//3ºEVAL
-            foreach($cursos as $curso){
-                foreach($curso->subjects as $j=>$subject){
-                    DB::table('yearUnions')->insert([
-                        'subject_id' => $subject->id,
-                        'course_id' => $curso->id,
-                        'evaluation_id' => $i,
-                        'year_id' => $year,
-                        'date_start'=> $fechasInicioFin[$i-1]['date_start'],
-                        'date_end'=> $fechasInicioFin[$i-1]['date_end'],
-                        'classroom_id'=> $j+1, 
-                        'created_at' => now(),
-                        'updated_at' => now(),
-                    ]);
+            
+            if( $i==3 && ($curso->level == 2)){  
+                break;
+            }else{
+                foreach($cursos as $curso){
+                    foreach($curso->subjects as $j=>$subject){
+                        DB::table('yearUnions')->insert([
+                            'subject_id' => $subject->id,
+                            'course_id' => $curso->id,
+                            'evaluation_id' => $i,
+                            'year_id' => $year,
+                            'date_start'=> $fechasInicioFin[$i-1]['date_start'],
+                            'date_end'=> $fechasInicioFin[$i-1]['date_end'],
+                            'classroom_id'=> $j+1, 
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                        ]);
+                    }
                 }
             }
-            if($curso->level == 2){
-                if( $i==2 ){  
-                    break;
-                }
-            }
+            
+            
         }
         
 
