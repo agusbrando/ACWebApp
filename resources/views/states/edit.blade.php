43f97101fe92565489f3a1b1d09dc9c8e47d3076
kehocu <kehocu@campusaula.com>
2020-05-22 14:18:55 +0200

    Clean code and fix CRUD & views of States

--- resources/views/states/edit.blade.php
+++ resources/views/states/edit.blade.php
@@ -1,10 +1,7 @@
 @extends('base')
 
 @section('main')
-
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
- 
-
     <div class="card shadow">
         <div class="card-header row m-0 justify-content-between">
             <h3>Detalles Estados</h3>
@@ -24,7 +21,6 @@
             </div>
             <div class="col-12 col-md-8 col-lg-10 p-3">
                 <div>
-
                     <fieldset>
                         <div class="form-group">
                             <label for="name">Nombre</label>
@@ -37,29 +33,7 @@
                 </div>
             </div>
         </div>
-
         </form>
     </div>
 </main>
 @endsection
-
-
-<!-- Rutas -->
-
-<!-- Route::resource('users','UserController');
-Route::get('users/edit/{id}',['as' => 'users.showedit', 'uses' => 'UserController@show']); -->
-
-<!-- Controller -->
-
-<!-- public function show($user_id)
-    {
-        $user = User::find($user_id);
-        $edit = false;
-        if(URL::current() == url("/users/edit/".$user_id)){
-            $edit = true;
-        }
-        return view('users.show', compact('user','edit'));
-    } -->
-<!-- Import de URL -->
-<!-- use Illuminate\Support\Facades\URL; -->
-<!--  -->
