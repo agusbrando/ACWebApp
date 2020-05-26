b3382c74fde1c47a9905aa59d7ea0c9fd289b090
Sergio <serlomar@campusaula.com>
2020-05-26 10:03:46 +0200

    fix-lineas borradas

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,54 +1,52 @@
 <header>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">Aula Campus</a>
-        <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
-        <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-        <div class="notificationsDropDown">
-            <div class="dropdown show">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="far fa-bell dropDownIcon mr-5"></i>
-                </a>
+  <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">Aula Campus</a>
+    <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Authentication Links -->
+      <div class="notificationsDropDown">
+        <div class="dropdown show">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="far fa-bell dropDownIcon mr-5"></i>
+          </a>
 
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    @foreach (Session::get('notifications') as $notification)
-                        <a class="dropdown-item" href="{{ route('messages.show',$notification->data['message_id'])}}">Tienes un nuevo mensaje de: {{$notification->data['email']}}</a>
-                    @endforeach
-                </div>
-              </div>
-
-
-
-            @guest
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            @if (Route::has('register'))
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Registrate') }}</a>
-            </li>
-            @endif
-            @else
-            <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}<span class="caret"></span>
-                </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" >
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                        {{ __('Desconectarse') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
-            </li>
-            @endguest
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            
+          </div>
         </div>
-        </ul>
-    </nav>
+
+
+
+        @guest
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @if (Route::has('register'))
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">{{ __('Registrate') }}</a>
+        </li>
+        @endif
+        @else
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}<span class="caret"></span>
+          </a>
+
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+              {{ __('Desconectarse') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+        @endguest
+      </div>
+    </ul>
+  </nav>
 </header>
 {{-- <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
