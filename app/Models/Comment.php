1b1b48fd1eb38e24fb0d8a90153f4fe39592fc36
adpesan <aperezsisagri@hotmail.com>
2020-05-27 09:47:38 +0200

    Models adpesan: añadir use por si acaso

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
+use App\Models\User;
 
 class Comment extends Model
 {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
+use App\Models\Attachment;
+use App\Models\User;
 
 class Post extends Model
 {
